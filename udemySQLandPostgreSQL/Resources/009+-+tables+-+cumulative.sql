331c938ad7714f540a86f317cf9770798894bda4
Jordan Clayton <jordan_clayton@protonmail.com>
2023-01-18 12:42:02 -0500

    sql resources

--- udemySQLandPostgreSQL/Resources/009+-+tables+-+cumulative.sql
+++ udemySQLandPostgreSQL/Resources/009+-+tables+-+cumulative.sql
@@ -28,3 +28,51 @@ VALUES
   ('http://754.jpg', 2),
   ('http://35.jpg', 3),
   ('http://256.jpg', 4);
+
+  /* find all the photos for user with id 1 */
+  SELECT * FROM photos WHERE user_id = 1;
+
+  /* list all photos with details about the associated user for each */
+  SELECT * FROM photos
+  JOIN users ON photos.user_id = users.id;
+
+boat_id INTEGER REFERENCES boats (id),
+
+SELECT * FROM crew_members WHERE boat_id = 1;
+
+/* exercise 8 */
+
+-- Create table called 'boats'
+CREATE TABLE boats (
+    -- Note that this environment doesn't support 'SERIAL' keyword
+    -- so 'AUTOINCREMENT' is used instead. Postgres always uses 'SERIAL'
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR
+);
+
+
+-- Insert two boats 
+INSERT INTO boats (name)
+VALUES ('Rogue Wave'), ('Harbor Master');
+
+
+-- Create table called 'crew_members'
+CREATE TABLE crew_members (
+    -- Note that this environment doenst support 'SERIAL' keyword
+    -- so 'AUTOINCREMENT' is used instead. Postgres always uses 'SERIAL'
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR,
+    -- >>>>>>>> TODO #1 HERE!!!
+    boat_id INTEGER REFERENCES boats (id)
+    
+);
+
+-- Insert three crew members
+INSERT INTO crew_members (first_name, boat_id)
+VALUES ('Alex', 1), ('Lucia', 1), ('Ari', 2);
+
+
+-- Write query here to fetch all columns for all crew_members associated with the boat that has an ID of 1
+-- >>>>>>>> TODO #2 HERE!!!
+
+SELECT * FROM crew_members WHERE boat_id = 1;
